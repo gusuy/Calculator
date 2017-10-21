8ebfdb8ff94e278bc5813e1ebecf0c4841709ec1
Gus Uy <gus.mgu@gmail.com>
2017-10-21 14:50:09 -0400

    Perform pending binary op when adding new binary ops

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -72,6 +72,10 @@ struct CalculatorBrain {
                 }
             case .binaryOperation(let function):
                 if accumulator != nil {
+                    if resultIsPending {
+                        performPendingBinaryOperation()
+                    }
+                    
                     pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
                     accumulator = nil
                 }
